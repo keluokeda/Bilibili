12260ba53c8da41e628cde1c82c7ad9c3f079390
keluokeda <913918146@qq.com>
2025-04-25 09:08:22 +0800

    本次更新内容： 1，发布新版本。

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ke.bilibili.tv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.2.3"
+        versionCode = 24
+        versionName = "1.2.4"
 
 
     }
